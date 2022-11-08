eb797c5cd369038c57bd16d4caf20a88f488430d
Kirill Mil <kirill.mil.0310@mail.ru>
2022-11-08 22:38:00 +0700

    main script

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-from constants import TELEGRAM_TOKEN
+from constants import TELEGRAM_TOKEN, START_CMD, HELP_CMD, EXAM_INFO, NOT_EXAMS, EXAMS_ARE_EXIST, SPLIT_LINE
+from service import get_session_timetable
 
 
 logging.basicConfig(
@@ -15,12 +16,33 @@ bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await bot.send_message(message.chat.id, START_CMD)
+
+
+@dp.message_handler(commands=['help'])
+async def help(message: types.Message):
+    await bot.send_message(message.chat.id, HELP_CMD)
+
+
+@dp.message_handler()
+async def session(message: types.Message):
+    exams = get_session_timetable(message.text)
+    if exams is None:
+        return await bot.send_message(message.chat.id, NOT_EXAMS.format(group_name=message.text))
+    result_message = EXAMS_ARE_EXIST.format(group_name=message.text)
+    for exam in exams:
+        result_message += EXAM_INFO.format(
+            subject_name=exam['subject'][1],
+            professor=exam['professor_name'],
+            date=exam['date'],
+            time=exam['time'],
+            room=exam['room'],
+            day_week=exam['day_week']
+        )
+        result_message += SPLIT_LINE
+    return await bot.send_message(message.chat.id, result_message)
 
 
 if __name__ == '__main__':
