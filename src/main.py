0c20f3ee1f937de580ea1a275238dd15dea56951
Kirill Mil <kirill.mil.0310@mail.ru>
2022-11-08 18:21:34 +0700

    simple bot for test

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+from constants import TELEGRAM_TOKEN
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+)
+
+
+bot = Bot(token=TELEGRAM_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
