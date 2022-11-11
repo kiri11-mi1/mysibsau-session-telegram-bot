e03923f85a7c1e7f8820692401c6857c978880ef
Kirill Mil <kirill.mil.0310@mail.ru>
2022-11-11 22:53:02 +0700

    add exeption

--- src/service/session.py
+++ src/service/session.py
@@ -1,13 +1,18 @@
 import requests
 
-from constants import SESSION_BACKEND_HOST, HTTP_200_OK
+from constants import SESSION_BACKEND_HOST, HTTP_200_OK, HTTP_400_BAD_REQUEST, SERVER_INTERNAL_ERROR
 
 
-def get_session_timetable(group) -> str:
-    response = requests.get(
-        f'{SESSION_BACKEND_HOST}/session/{group}'
+def get_session_timetable(group) -> [str, str]:
+    try:
+        response = requests.get(
+            f'{SESSION_BACKEND_HOST}/session/{group}',
+            timeout=10
+        )
+    except ConnectionError:
+        return None, SERVER_INTERNAL_ERROR
 
-    )
     if response.status_code == HTTP_200_OK:
-        return response.json()
-
+        return response.json(), None
+    if response.status_code == HTTP_400_BAD_REQUEST:
+        return None, response.json()['message']
