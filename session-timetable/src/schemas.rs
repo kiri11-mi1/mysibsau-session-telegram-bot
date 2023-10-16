ac4a00fb0ad79c89526a10434eb14eaadcd5931c
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-10-16 10:13:05 +0700

    add schemas

---
+++ session-timetable/src/schemas.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Group {
+    pub id: i64,
+    pub name: String,
+}
+
+impl From<Map<String, Value>> for Group {
+    fn from(value: Map<String, Value>) -> Self {
+        Self {
+            id: value["id"].as_i64().unwrap(),
+            name: value["name"].to_string(),
+        }
+    }
+}
+
+pub struct GroupList {
+    pub data: Vec<Group>,
+}
+
+impl From<Vec<Map<String, Value>>> for GroupList {
+    fn from(items: Vec<Map<String, Value>>) -> Self {
+        let mut result = vec![];
+        for v in items {
+            result.push(Group::from(v))
+        }
+        return GroupList { data: result };
+    }
+}
