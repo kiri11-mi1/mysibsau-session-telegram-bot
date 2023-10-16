dd5489687923fd2be1bebc552b370d0543ea7685
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-10-16 22:39:37 +0700

    use msg stubs

--- session-timetable/src/main.rs
+++ session-timetable/src/main.rs
@@ -1,9 +1,10 @@
+mod msg_templates;
 mod pallada_service;
 mod schemas;
 
+use msg_templates::*;
 use pallada_service::PalladaService;
 use teloxide::prelude::*;
-
 
 #[tokio::main]
 async fn main() {
@@ -20,29 +21,33 @@ async fn main() {
 async fn answer(bot: Bot, msg: Message) -> ResponseResult<()> {
     let msg_text = msg.text();
     if msg_text.is_none() {
-        bot.send_message(msg.chat.id, "Не понимаю. Введите текстовое сообщение.")
-            .await?;
+        bot.send_message(msg.chat.id, DEFAULT_RESPONSE).await?;
         return Ok(());
     }
     match msg_text.unwrap() {
-        "/start" => bot.send_message(msg.chat.id, "Привет, чтобы узнать расписание сессии, просто введи название своей группы. Регистр не важен.").await?,
+        "/start" => bot.send_message(msg.chat.id, START).await?,
+        "/help" => bot.send_message(msg.chat.id, HELP).await?,
         _ => {
             let exams = search_exams(msg_text.unwrap()).await;
-            bot.send_message(msg.chat.id, exams).await?   
+            bot.send_message(msg.chat.id, exams).await?
         }
     };
     Ok(())
 }
 
 async fn search_exams(group: &str) -> String {
-    let mut pds = PalladaService::new().await;
-    let group_id = pds.find_group_by_name(String::from(group)).await;
-    if group_id.is_none() {
-        return "Группа с таким именем не найдена.".to_string();
+    let pds = PalladaService::new().await;
+    if pds.is_none() {
+        return SERVER_ERROR.to_string();
     }
-    let timetable_list = pds.get_exams_timetable(group_id.unwrap()).await;
+    let mut pds_unwrapped = pds.unwrap();
+    let group_id = pds_unwrapped.find_group_by_name(String::from(group)).await;
+    if group_id.is_none() {
+        return GROUP_NOT_FOUND.to_string();
+    }
+    let timetable_list = pds_unwrapped.get_exams_timetable(group_id.unwrap()).await;
     if timetable_list.is_none() {
-        return "Расписание экзаменов пока отсуствует.".to_string();
+        return EXAMS_DO_NOT_EXIS.to_string();
     }
     let exams = timetable_list.unwrap();
     for exam in exams {
@@ -50,5 +55,5 @@ async fn search_exams(group: &str) -> String {
     }
 
     let result_text = format!("Группа {}.", group);
-    return result_text
+    return result_text;
 }
