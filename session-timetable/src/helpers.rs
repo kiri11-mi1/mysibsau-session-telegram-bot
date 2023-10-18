3eab244c1fea0a0186c2a68c73f049168bcd11f7
kiri11-mi1 <kirill.mil.0310@mail.ru>
2023-10-18 17:15:55 +0700

    delete time helper and add new helper

--- session-timetable/src/helpers.rs
+++ session-timetable/src/helpers.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 pub fn fetch_day_name(number_day: &str) -> String {
     match number_day {
         "1" => "Понедельник".to_string(),
@@ -10,6 +12,14 @@ pub fn fetch_day_name(number_day: &str) -> String {
     }
 }
 
-pub fn fetch_normal_time(time_value: &str) -> String {
-    return time_value.replace("-", "");
+pub fn fetch_lesson_name(lesson: Value) -> String {
+    let lesson_arr = lesson.as_array();
+    if lesson_arr.is_none() {
+        return "".to_string();
+    }
+    let lesson_arr = lesson_arr.unwrap();
+    if lesson_arr.len() != 2 {
+        return "".to_string();
+    }
+    return lesson_arr[1].to_string().replace("\"", "");
 }
